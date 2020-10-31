d0b48725ce5eab4c18dcb1dce64b338fb8f77ee7
Pablo Ivancich <pabloivancich@gmail.com>
2020-10-31 01:55:14 -0300

    Project spring boot starter with datasource and actuator configuration.

---
+++ src/test/java/com/pidev/minesweeperapi/MinesweeperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pidev.minesweeperapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
